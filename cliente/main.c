54f7cf6146ec6adf32a42559aa24ab501d259305
Alessandro Camillo Gimenez de Menezes <alessandrocamillogimenezdemenezes@Alessandro-Mack.local>
2014-03-14 21:01:07 -0300

    Initial Commit

---
+++ cliente/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  cliente
+//
+//  Created by Alessandro Camillo Gimenez de Menezes on 14/03/14.
+//  Copyright (c) 2014 Alessandro Camillo Gimenez de Menezes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
